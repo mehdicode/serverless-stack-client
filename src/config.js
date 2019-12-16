8a6e391f1798921a91db205b6d8fd511cd774ecd
Mahdi Kalan <kalan.mahdi@yahoo.com>
2019-12-15 18:32:11 -0800

    set up aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-1",
+      BUCKET: "mserver-test2"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://ns1lel9mti.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_aoJejMqYU",
+      APP_CLIENT_ID: "20ibplnn3pk328s5a0aa5thujr",
+      IDENTITY_POOL_ID: "99ffce36-aaad-49a2-a6a4-0ca17c99f3dc"
+    }
+  };
