8e9f8f2df777384784f0f56ef08ffa65d73a2555
Mahdi Kalan <kalan.mahdi@yahoo.com>
2019-12-15 18:54:00 -0800

    Add the Session to the State

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -20,6 +20,7 @@ export default function Login(props) {
   
     try {
       await Auth.signIn(email, password);
+      props.userHasAuthenticated(true);
       alert("Logged in");
     } catch (e) {
       alert(e.message);
