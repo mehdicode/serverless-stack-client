47606b14ced6f008e20be17df93ef2f0a7ed54db
Mahdi Kalan <kalan.mahdi@yahoo.com>
2019-12-15 17:39:16 -0800

    added react router and landing page

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  );
+}
+
+// This component uses this Switch component from React-Router that renders the first matching route that is defined within it. 
+// For now we only have a single route, it looks for / and renders the Home component when matched. We are also using the exact 
+// prop to ensure that it matches the / route exactly. This is because the path / will also match any route that starts with a /.
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Use BrowserRouter as our router. This uses the browser’s History API to create real URLs.
+// Use the Router to render our App component. This will allow us to create the routes we need inside our App component.
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
